d60bf306ebd1fc134be92754234241b796878757
Phurinat <phurinat_bunyachai@hotmail.com>
2022-07-03 20:03:09 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div class="head-section">
+      <span class="keyword">{{keyword ? keyword : 'กินอะไรดี'}}</span>
+    </div>
+    
+      <div class="restaurant-section">
+        <div class="search">
+          <input 
+            type="text" 
+            :value="keyword" 
+            :placeholder="keyword ? keyword : 'กินอะไรดี' " 
+            @keyup="(e) => onSearch({value:e.target.value})"
+            />
+        </div>
+         <div class="restaurant">
+          <Restaurant :restaurants="restaurants" />
+        </div>
+      </div>
+    
+  </div>
+</template>
+
+<script>
+import Restaurant from './components/restaurant/List.vue'
+import { ref,onMounted } from 'vue'
+export default {
+  components: {
+    Restaurant
+  },
+
+  setup() {
+    let restaurants = ref([{}])
+    let keyword = ref('bang sue')
+    onMounted(() => {
+      restaurants.value = [{name: 'test'},{name: 'eiei'},{name: 'eiei22'}]
+    })
+
+    function onSearch({value}){
+      keyword.value = value
+    }
+
+
+    return{restaurants,keyword,onSearch}
+  }
+
+}
+</script>
+
+<style>
+  @import './assets/css/restaurant.css';
+</style>
---
+++ src/components/restaurant/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='restaurant-list' v-for="(restaurant, index) in restaurants" :key="index">
+     <div class='restaurant-image'>
+        <img src='https://placehold.jp/650x350.png'/>
+     </div>
+     <span class='restaurant-name'>{{restaurant.name}}</span>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+    name:'restaurants-list',
+    props:{
+        restaurants: Array
+    },
+
+    setup(){
+      onMounted(() => {
+    })
+  }
+}
+</script>
+
+<style>
+  @import './index.css';
+</style>
+
